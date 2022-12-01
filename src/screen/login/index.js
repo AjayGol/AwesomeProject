866f34076855bb2176311b0d25e9771d9450f74b
AjayGol <ajaygoldev@gmail.com>
2022-12-01 16:30:12 +0530

    Fixed api issue

--- src/screen/login/index.js
+++ src/screen/login/index.js
@@ -27,6 +27,7 @@ export default function LoginScreen() {
   const navigation: any = useNavigation();
 
   const onPressLogin = () => {
+    navigation.navigate(UserStack.tabBar);
     if (email === '' && password === '') {
       alert('Please enter detail');
     } else if (email === '') {
